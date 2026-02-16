621c598d664b615fdb273c4e59bd359ebe56b954
Divyasri-M-2006 <divyasri.m28@gmail.com>
2026-02-16 11:10:58 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+    public static void main(String args[]){
+        double num1=10;
+        double num2=5;
+        System.out.println("Addition: "+(num1+num2));
+        System.out.println("Subtraction: "+(num1-num2));
+        System.out.println("Multiplication: "+(num1*num2));
+        System.out.println("Division: "+(num1/num2));
+    }
+}
